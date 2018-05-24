cd876edceaaf7305a984462b025f909a7d4343b5
hinsheva <38138872+hinsheva@users.noreply.github.com>
2018-05-24 13:13:38 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,20 @@
 public class Calculator {
+    public Calculator(){
+    }
+
+    public double add(double first, double second){
+        return first + second;
+    }
+
+    public float subtract(float first, float second){
+        return first - second;
+    }
+
+    public int multiply(int first, int second){
+        return first*second;
+    }
+
+    public int divide(int first, int second){
+        return first/second;
+    }
 }
