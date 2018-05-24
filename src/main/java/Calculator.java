7b9953bdc68f4a6cb97a3778f317854aa444610b
valeriiborovyk <borovykvalerii@gmail.com>
2018-05-24 21:38:47 +0300

    Homework 1 calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,25 @@
 public class Calculator {
+    static int additionReworked(byte number1, short number2) {
+        return number1+number2;
+    }
+    static int subtractionReworked(byte number1, short number2) {
+        return number1-number2;
+    }
+    static int multiplicationReworked(byte number1, short number2) {
+        return number1*number2;
+    }
+    static int divisionReworked(byte number1, short number2) {
+        int result = 0;
+        try {
+            return result = number1/number2;
+        }
+        catch (ArithmeticException exception)
+        {
+            System.out.println("An invalid arithmetic operation occurred");
+        }
+        finally
+        {
+            return result;
+        }
+    }
 }
