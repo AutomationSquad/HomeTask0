9b874433f25ba81264038a4e25e3778cec157242
Vitalii <vitalii.sotnichenko@gmail.com>
2018-05-27 19:36:10 +0300

    Calculator

---
+++ src/main/java/HomeTask0/Calculator.java
@@ -0,0 +1,201 @@
+package HomeTask0;
+
+public class Calculator {
+
+    public static void main (String args[]) {
+
+        int ai1 = 2;
+        int ai2 = 3;
+        int ai3 = ai1 + ai2;
+
+        Integer aii1 = 2;
+        Integer aii2 = 4;
+        Integer aii3 = aii1 + aii2;
+
+        byte ab1 = 6;
+        byte ab2 = 3;
+        byte ab3 = (byte)(ab1 + ab2);
+
+        Byte abb1 = 11;
+        Byte abb2 = 22;
+        Byte abb3 = (byte)(abb1 + abb2);
+
+        short as1 = 205;
+        short as2 = 305;
+        short as3 = (short)(as1 + as2);
+
+        Short ass1 = 30;
+        Short ass2 = 5;
+        Short ass3 = (short)(ass1 + ass2);
+
+        long al1 = 1000L;
+        long al2 = 1500L;
+        long al3 = al1 + al2;
+
+        Long all1 = 2500L;
+        Long all2 = 500L;
+        Long all3 = all1 + all2;
+
+        float af1 = 3.5f;
+        float af2 = 5.55f;
+        float af3 = af1 + af2;
+
+        Float aff1 = 55.5f;
+        Float aff2 = 44.4f;
+        Float aff3 = aff1 + aff2;
+
+        double ad1 = 44.55;
+        double ad2 = 3.5;
+        double ad3 = ad1 + ad2;
+
+        Double add1 = 33.44;
+        Double add2 = 44.55;
+        Double add3 = add1 + add2;
+
+
+        System.out.println(ai3);
+        System.out.println(aii3);
+        System.out.println(ab3);
+        System.out.println(abb3);
+        System.out.println(as3);
+        System.out.println(ass3);
+        System.out.println(al3);
+        System.out.println(all3);
+        System.out.println(af3);
+        System.out.println(aff3);
+        System.out.println(ad3);
+        System.out.println(add3);
+
+        int di1 = 7;
+        int di2 = 3;
+        int di3 = (di1/di2);
+
+        Integer dii1 = 8;
+        Integer dii2 = 4;
+        Integer dii3 = (dii1/dii2);
+
+        byte db1 = 6;
+        byte db2 = 3;
+        byte db3 = (byte)(db1/db2);
+
+        Byte dbb1 = 60;
+        Byte dbb2 = 20;
+        Byte dbb3 = (byte)(dbb1/dbb2);
+
+        short ds1 = 20;
+        short ds2 = 30;
+        short ds3 = (short)(ds1/ds2);
+
+        Short dss1 = 30;
+        Short dss2 = 5;
+        Short dss3 = (short)(dss1/dss2);
+
+        long dl1 = 300L;
+        long dl2 = 150L;
+        long dl3 = (dl1/dl2);
+
+        Long dll1 = 25L;
+        Long dll2 = 5L;
+        Long dll3 = (dll1/dll2);
+
+        float df1 = 3.0f;
+        float df2 = 1.5f;
+        float df3 = (df1/df2);
+
+        Float dff1 = 5.5f;
+        Float dff2 = 2.0f;
+        Float dff3 = (dff1/dff2);
+
+        double dd1 = 10.5;
+        double dd2 = 2.5;
+        double dd3 = (dd1/dd2);
+
+        Double ddd1 = 30.5;
+        Double ddd2 = 5.0;
+        Double ddd3 = (ddd1/ddd2);
+
+
+
+        System.out.println(di3);
+        System.out.println(dii3);
+        System.out.println(db3);
+        System.out.println(dbb3);
+        System.out.println(ds3);
+        System.out.println(dss3);
+        System.out.println(dl3);
+        System.out.println(dll3);
+        System.out.println(df3);
+        System.out.println(dff3);
+        System.out.println(dd3);
+        System.out.println(ddd3);
+
+
+        int i1 = 2;
+        int i2 = 3;
+        int i3 = i1 * i2;
+
+        Integer ii1 = 2;
+        Integer ii2 = 4;
+        Integer ii3 = ii1 * ii2;
+
+        byte b1 = 6;
+        byte b2 = 3;
+        byte b3 = (byte)(b1 * b2);
+
+        Byte bb1 = 10;
+        Byte bb2 = 20;
+        Byte bb3 = (byte)(bb1 * bb2);
+
+        short s1 = 20;
+        short s2 = 30;
+        short s3 = (short)(s1 * s2);
+
+        Short ss1 = 30;
+        Short ss2 = 5;
+        Short ss3 = (short)(ss1 * ss2);
+
+        long l1 = 100L;
+        long l2 = 150L;
+        long l3 = l1 * l2;
+
+        Long ll1 = 25L;
+        Long ll2 = 5L;
+        Long ll3 = ll1 * ll2;
+
+        float f1 = 2.5f;
+        float f2 = 1.5f;
+        float f3 = f1 * f2;
+
+        Float ff1 = 5.5f;
+        Float ff2 = 3.4f;
+        Float ff3 = ff1 * ff2;
+
+        double d1 = 44.55;
+        double d2 = 3.5;
+        double d3 = d1 * d2;
+
+        Double edd1 = 33.44;
+        Double edd2 = 44.55;
+        Double edd3 = (edd1 * edd2);
+
+
+
+        System.out.println(i3);
+        System.out.println(ii3);
+        System.out.println(b3);
+        System.out.println(bb3);
+        System.out.println(s3);
+        System.out.println(ss3);
+        System.out.println(l3);
+        System.out.println(ll3);
+        System.out.println(f3);
+        System.out.println(ff3);
+        System.out.println(d3);
+        System.out.println(edd3);
+
+
+    }
+
+
+    }
+
