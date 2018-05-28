1b11e60ce385f3828f75b2aa6e16f4669a44c255
IrynaSemenovska <semenovskaya.iryna@gmail.com>
2018-05-28 13:18:22 +0300

    Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,24 @@
-public class Calculator {
+class Calculator {
+    public static double summOperation(double s, double s1) {
+        return s + s1;
+    }
+
+    public static double subtractionOperation(double m, double m1) {
+        return m - m1;
+    }
+
+    public static double multiplicationOperation(double t, double t1) {
+        return t * t1;
+    }
+
+    public static int divisionOperation(int d, int d1) {
+            return d / d1;
+    }
+    public static double division1Operation(double b, double b1) {
+        return b / b1;
+    }
+
+    public static double sqrtOperation(double q) {
+            return Math.sqrt(q);
+    }
 }
