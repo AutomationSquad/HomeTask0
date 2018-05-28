4f830edfdc5eb1495b43c6fb283a2d4ade6dfdb0
Lagod <lagodsanya@gmail.com>
2018-05-28 17:26:24 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,21 @@
 public class Calculator {
+  
+    public  Calculator() {
+    }
+
+    public int add(int number, int number2){
+        return number + number2;
+    }
+    public int subsract(int number, int number2){
+        return number- number2;
+    }
+
+    public int mult(int number, int number2){
+        return number * number2;
+    }
+
+    public double division(int number, int number2){
+        return number / number2;
+    }
 }
+
