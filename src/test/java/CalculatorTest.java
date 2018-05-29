61b01880436b2680f3fbd174842df455884ae663
vitaliisotnichenko <vitalii.sotnichenko@gmail.com>
2018-05-30 00:05:26 +0300

    Update assertions

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,26 +1,36 @@
+import HomeTask0.Calculator;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class CalculatorTest {
 
+
+    private int ai1 = 2;
+    private int ai2 = 3;
+    private byte ab1 = 6;
+    private byte ab2 = 3;
+    private short as1 = 21;
+    private short as2 = 4;
+    private float af1 = 5.0f;
+    private float af2 = 2.5f;
+
+
     @Test
 
-    public void checSumTest() {
+    public void checkAddSumTest() {
 
-        int ia = 3;
-        int ib = 5;
-
-        Assert.assertEquals("Check your arguments", ia + ib, 8);
+        int checkSum = Calculator.addition(ai1, ai2);
+        System.out.println("Sum two values " + ai1 + "+" + ai2 + "=" + checkSum);
+        Assert.assertEquals("Addition of values is incorrect. Check your values", 5, checkSum);
     }
 
     @Test
 
     public void checkSubtractTest() {
 
-        double d1 = 5.55;
-        double d2 = 3.33;
-
-        Assert.assertEquals(d1 - d2, 2.22, 3);
+        byte checkSubtract = Calculator.subtraction(ab1, ab2);
+        System.out.println("Subtract two values " + ab1 + "-" + ab2 + "=" + checkSubtract);
+        Assert.assertEquals("Subtract two values is incorrect. Check your values", 3, checkSubtract);
 
     }
 
@@ -28,10 +38,9 @@ public class CalculatorTest {
 
     public void divideCheck() {
 
-        int ii1 = 11;
-        int ii2 = 2;
-
-        Assert.assertEquals("Check your arguments", ii1 / ii2, 5);
+        float checkDivide = Calculator.divide(af1, af2);
+        System.out.println("Divide two values " + af1 + "/" + af2 + "=" + checkDivide);
+        Assert.assertEquals(2.0, af1 / af2, 2);
     }
 
 
@@ -39,124 +48,34 @@ public class CalculatorTest {
 
     public void checkMult() {
 
-        byte i = 2;
-        byte b = 20;
-
-        Assert.assertEquals("Check your arguments", i / b, 0);
+        short checkMultiplication = Calculator.multiplication(as1, as2);
+        System.out.println("Multiplication two values " + as1 + "/" + as2 + "=" + checkMultiplication);
+        Assert.assertEquals("Multiplication two values is incorrect. Check your values", 84, checkMultiplication);
 
 
     }
 
     @Test
 
-    public void percentCheck() {
+    public void remainderCheck() {
 
-        short s1 = 9;
-        short s2 = 4;
-
-        Assert.assertEquals("Check your arguments", s1%s2, 1);
+        short checkRemainderCheck = Calculator.remainder(as1, as2);
+        System.out.println("Remainder two values " + as1 + "%" + as2 + "=" + checkRemainderCheck);
+        Assert.assertEquals("Remainder two values is incorrect. Check your values", as1 % as2, checkRemainderCheck);
     }
 
 
     @Test
 
-    public void addition1Check() {
-
-        int i = 25;
-
-        Assert.assertEquals("Check your arguments", i+=1, 26);
-    }
+    public void negAddition2Check() {
 
 
-    @Test
-
-    public void subtraction1Check() {
-
-        long l = 4333333L;
-
-        Assert.assertEquals("Check your arguments", l -=1, 4333332L);
+        int negAddition = Calculator.addition(ai1, ai2);
+        System.out.println("Sum two values " + ai1+ "+" + ai2 + "=" + negAddition);
+        Assert.assertEquals("Addition two values is incorrect. Check your values", 6, negAddition);
 
     }
 
-    @Test
-
-    public void mult1Check() {
-
-        int i = 22;
-        Assert.assertEquals("Check your arguments", i *= 3, 66);
-
-    }
-    @Test
-
-    public void addition3Check() {
-
-        int a = -5;
-        int b = -3;
-
-        Assert.assertEquals("Check your arguments", a+b, -8);
-    }
-
-    @Test
-
-    public void negDivide1Check() {
-
-        float f1 = 20.5f;
-
-        Assert.assertEquals(f1 /= 2, 1.25, 2);
-    }
-
-    @Test
-
-    public void negDivide2Check() {
-
-
-        byte b = 8;
-        byte c = -2;
-
-        Assert.assertEquals("Check your arguments", b/c, 4);
-
-    }
-
-    @Test
-
-    public void negAddition2Check () {
-
-        byte b = 127;
-
-        Assert.assertEquals("Check your arguments", b+=1, 128);
-
-    }
-
-    @Test
-
-    public void negaddition2Check() {
-
-        int a = -5;
-        int b = -3;
-
-        Assert.assertEquals("Check your arguments", a+b, 2);
-    }
-
-    @Test
-
-    public void neg3MulCheck() {
-
-        byte b = -3;
-        byte d = -2;
-
-        Assert.assertEquals("Check your arguments", b*d, -6);
-
-    }
-
-    @Test
-
-    public void neg3DivideCheck() {
-
-        byte b = 4;
-        byte d = 2;
-
-        Assert.assertEquals("Check your arguments", b/d, 3);
-    }
 
     @Test
 
@@ -165,7 +84,10 @@ public class CalculatorTest {
         int i = 44;
         int b = 0;
 
-        Assert.assertEquals(i/b, 0);
+        float divideZero = Calculator.divide(i, b);
+        System.out.println("Divide on zero " + i + "/" + b + "=" + divideZero);
+        Assert.assertEquals(0, divideZero, 1);
     }
+
 }
 
