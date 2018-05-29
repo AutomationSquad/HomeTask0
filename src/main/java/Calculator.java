60bd3122147b079c4906986d30ccc6c8761c51b9
sgavrilyuk <gavrilyuk.pb@gmail.com>
2018-05-29 23:38:17 +0300

    The First Hometask

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,44 @@
+import java.util.Scanner;
+
 public class Calculator {
+
+    static int x = 10;
+    static int y = 3;
+    static int z = 0;
+
+    public static void main(String[] args) {
+        System.out.println("a + b = " + addition());
+        System.out.println("a - b = " + subtraction());
+        System.out.println("a * b = " + multiplication());
+        System.out.println("a / b = " + division());
+        System.out.println("a % b = " + modulo());
+        System.out.println("Have a good day");
+    }
+
+   static int addition(){
+       return x + y;
+    }
+
+    static  int subtraction(){
+        return x - y;
+    }
+
+    static  double multiplication(){
+        return x * y;
+    }
+
+    static  Double division(){
+        if (y != 0){
+            return (double)x / y;
+        }
+        return null;
+    }
+
+    static Double modulo(){
+        if (z != 0){
+            return (double)x % z;
+        }
+        return null;
+    }
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,44 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class CalculatorTest {
+
+    @Test
+    public void addTest(){
+        assertEquals("incorrect", Calculator.addition(), Calculator.x + Calculator.y);
+    }
+
+    @Test
+    public void addWrongTest(){
+        assertEquals("incorrect addition", Calculator.addition(), Calculator.x - Calculator.y);
+    }
+
+    @Test
+    public void subTest(){
+        assertEquals("incorrect", Calculator.subtraction(), Calculator.x - Calculator.y);
+    }
+    @Test
+    public void multTest(){
+        assertEquals("incorrect", Calculator.multiplication(), Calculator.x * Calculator.y, 1e-8);
+    }
+    @Test
+    public void multWrongTest(){
+        assertEquals("incorrect multiplication", Calculator.multiplication(), Calculator.x + Calculator.y, 1e-8);
+    }
+    @Test
+    public void divTest(){
+        if (Calculator.y != 0) {
+        assertEquals(Calculator.division(), (double) Calculator.x / Calculator.y, 1e-8);
+        }
+        System.out.println("Impossible to run method - division. Division by zero ");
+    }
+    @Test
+    public void modTest() {
+        if (Calculator.z != 0) {
+            assertEquals(Calculator.modulo(), Calculator.x % Calculator.z, 1e-8);
+        }
+        System.out.println("Impossible to run method - modulo. Division by zero ");
+    }
 }
