fabe1e6a8a39f902f188166038b1e37a776ae202
Katerina Shepilova <k.shepilova@spd-ukraine.com>
2018-05-29 11:35:33 +0300

    Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,54 @@
 public class Calculator {
+    public double addition () {
+        double num1, num2, res1;
+        num1 = 1.2356;
+        num2 = 3.423;
+        res1 = num1+num2;
+        System.out.println("Op: + :" + res1);
+        return res1;
+    }
+    public int deduction () {
+        int num1, num2, res2;
+        num1 = -1;
+        num2 = 4;
+        res2 = num1-num2;
+        System.out.println("Op: - :" +res2);
+        return res2;
+    }
+
+    public double multiplication () {
+        double num1, res3;
+        int  num2;
+        num1 = 1.0000003;
+        num2 = 46;
+        res3 = num1*num2;
+        System.out.println("Op: * :" +res3);
+        return res3;
+    }
+
+    public double division () {
+        double num1, res4;
+        int num2;
+        num1 = 2.4;
+        num2 = 2;
+        res4 = num1/num2;
+        System.out.println("Op: / :" +res4);
+        return res4;
+    }
+    public double sqrt () {
+        double num1, res5;
+        num1 = 9;
+        res5 = Math.sqrt(num1);
+        System.out.println("Op: sqrt :" +res5);
+        return res5;
+    }
+
+    public static void main (String [] args){
+        Calculator calc = new Calculator();
+        double res1= calc.addition();
+        double res2= calc.deduction();
+        double res3= calc.multiplication();
+        double res4= calc.division();
+        double res5= calc.sqrt();
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,82 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class CalculatorTest {
+    //positive cases
+
+    @Test
+    public void addition() {
+        Calculator calc = new Calculator();
+        double actual = calc.addition();
+        assertEquals(4.6586, actual, 0);
+    }
+
+    @Test
+    public void deduction() {
+        Calculator calc = new Calculator();
+        int actual = calc.deduction();
+        assertEquals(-5, actual, 0);
+
+    }
+
+    @Test
+    public void multiplication() {
+        Calculator calc = new Calculator();
+        double actual = calc.multiplication();
+        assertEquals(46.0000138, actual, 0);
+    }
+
+    @Test
+    public void division() {
+        Calculator calc = new Calculator();
+        double actual = calc.division();
+        assertEquals(1.2, actual, 0);
+    }
+
+    @Test
+    public void sqrt_() {
+        Calculator calc = new Calculator();
+        double actual = calc.sqrt();
+        assertEquals(3, actual, 0);
+    }
+
+   //negative cases
+
+    @Test
+    public void addition_neg() {
+        Calculator calc = new Calculator();
+        double actual = calc.addition();
+        assertEquals(3, actual, 0);
+    }
+
+    @Test
+    public void deduction_neg() {
+        Calculator calc = new Calculator();
+        int actual = calc.deduction();
+        assertEquals(-1, actual, 0);
+
+    }
+
+    @Test
+    public void multiplication_neg() {
+        Calculator calc = new Calculator();
+        double actual = calc.multiplication();
+        assertEquals(2, actual, 0);
+    }
+
+    @Test
+    public void division_neg() {
+        Calculator calc = new Calculator();
+        double actual = calc.division();
+        assertEquals(0.5, actual, 0);
+    }
+
+    @Test
+    public void sqrt_neg() {
+        Calculator calc = new Calculator();
+        double actual = calc.sqrt();
+        assertEquals(1, actual, 0);
+    }
 }
+
