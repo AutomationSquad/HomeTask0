7b23a43f068985e3f954ae54e022f863c172faa8
nicholas <nicholasbin@gmail.com>
2018-05-29 19:26:08 +0300

    Home task1

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,18 @@
 public class Calculator {
+    static int numbers1 (int num1, int num2) {
+        return num1 + num2;
+    }
+
+    static int numbers2 (int num1, int num2) {
+        return num1 - num2;
+    }
+
+    static int number3 (int num1, int num2) {
+        return num1 * num2;
+    }
+
+    static int number4 (int num1, int num2) {
+        return num1 / num2;
+    }
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,53 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CalculatorTest {
+
+    @Test
+
+    public void Number1Test() {
+        int num1 = 10;
+        int num2 = 15;
+
+
+        Assert.assertEquals("Incorrect","25","25");
+    }
+
+    @Test
+
+    public void Number2Test1() {
+        int num1 = 17;
+        int num2 = 5;
+
+        Assert.assertEquals("Incorrect", "12", "12");
+    }
+
+    @Test
+
+    public void Number2Test2() {
+        int num1 = 15;
+        int num2 = 10;
+
+        Assert.assertEquals("Incorrect","12","5");
+    }
+
+    @Test
+
+    public void Number3Test() {
+        int num1 = 20;
+        int num2 = 10;
+
+        Assert.assertEquals("Incorrect","200","200");
+    }
+
+    @Test
+
+    public void Number4Test() {
+        int num1 = 33;
+        int num2 = 5;
+
+        Assert.assertEquals("Incorrect","6,6","6,6");
+
+    }
+
 }
