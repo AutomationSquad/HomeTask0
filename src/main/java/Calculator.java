f59836ce8eff5afc68a2f58f80f661e7509fd3e9
sgavrilyuk <gavrilyuk.pb@gmail.com>
2018-05-31 23:21:48 +0300

    The First Hometask

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,34 +7,34 @@ public class Calculator {
     static int z = 0;
 
     public static void main(String[] args) {
-        System.out.println("a + b = " + addition());
-        System.out.println("a - b = " + subtraction());
-        System.out.println("a * b = " + multiplication());
-        System.out.println("a / b = " + division());
-        System.out.println("a % b = " + modulo());
+        System.out.println("a + b = " + addition(x, y));
+        System.out.println("a - b = " + subtraction(x, y));
+        System.out.println("a * b = " + multiplication(x, y));
+        System.out.println("a / b = " + division(x, y));
+        System.out.println("a % b = " + modulo(x, y));
         System.out.println("Have a good day");
     }
 
-   static int addition(){
+   static int addition(int x, int y){
        return x + y;
     }
 
-    static  int subtraction(){
+    static  int subtraction(int x, int y){
         return x - y;
     }
 
-    static  double multiplication(){
+    static  double multiplication(int x, int y){
         return x * y;
     }
 
-    static  Double division(){
+    static  Double division(int x, int y){
         if (y != 0){
             return (double)x / y;
         }
         return null;
     }
 
-    static Double modulo(){
+    static Double modulo(int x, int y){
         if (z != 0){
             return (double)x % z;
         }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,41 +4,50 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
+    static int x = 12;
+    static int y = 5;
+    static int z = 0;
 
     @Test
-    public void addTest(){
-        assertEquals("incorrect", Calculator.addition(), Calculator.x + Calculator.y);
+    public void addTest() {
+        assertEquals("incorrect addition", Calculator.addition(x, y), x + y);
     }
 
     @Test
-    public void addWrongTest(){
-        assertEquals("incorrect addition", Calculator.addition(), Calculator.x - Calculator.y);
+    public void addWrongTest() {
+        assertEquals("incorrect addition", Calculator.addition(x, y), x - y);
     }
 
     @Test
-    public void subTest(){
-        assertEquals("incorrect", Calculator.subtraction(), Calculator.x - Calculator.y);
+    public void subTest() {
+        assertEquals("incorrect subtraction", Calculator.subtraction(x, y), x - y);
     }
+
     @Test
-    public void multTest(){
-        assertEquals("incorrect", Calculator.multiplication(), Calculator.x * Calculator.y, 1e-8);
+    public void multTest() {
+        assertEquals("incorrect multiplication", Calculator.multiplication(x, y), x * y, Double.MIN_VALUE);
     }
+
     @Test
-    public void multWrongTest(){
-        assertEquals("incorrect multiplication", Calculator.multiplication(), Calculator.x + Calculator.y, 1e-8);
+    public void multWrongTest() {
+        assertEquals("incorrect multiplication", Calculator.multiplication(x, y), x + y, Double.MIN_VALUE);
     }
+
     @Test
-    public void divTest(){
+    public void divTest() {
         if (Calculator.y != 0) {
-        assertEquals(Calculator.division(), (double) Calculator.x / Calculator.y, 1e-8);
+            assertEquals("incorrect division", Calculator.division(x, y), (double) x / y, Double.MIN_VALUE);
+        } else {
+            System.out.println("Impossible to run method - division. Division by zero ");
         }
-        System.out.println("Impossible to run method - division. Division by zero ");
     }
+
     @Test
     public void modTest() {
         if (Calculator.z != 0) {
-            assertEquals(Calculator.modulo(), Calculator.x % Calculator.z, 1e-8);
+            assertEquals("incorrect modulo", Calculator.modulo(x, y), x % z, Double.MIN_VALUE);
+        } else {
+            System.out.println("Impossible to run method - modulo. Division by zero ");
         }
-        System.out.println("Impossible to run method - modulo. Division by zero ");
     }
 }
